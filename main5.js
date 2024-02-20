ef3f3fcc94e59fad55a956c38084de2f9b9cb80f
imran1km <160271645+imran1km@users.noreply.github.com>
2024-02-20 23:37:27 +0500

    Add files via upload

---
+++ main5.js
@@ -0,0 +1,3 @@
+var famousPerson = " Quaid -e- Azam ";
+var massage = ("'frist Governor General Of Pakistan'");
+console.log(famousPerson, massage);
---
+++ main5.ts
@@ -0,0 +1,5 @@
+let famousPerson =" Quaid -e- Azam ";
+
+let massage = ("'frist Governor General Of Pakistan'");
+
+console.log (famousPerson, massage);
